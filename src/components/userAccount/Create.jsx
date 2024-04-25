e5cd363c03d529c14b76a3ffecc37f21c9fb4900
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-04-25 04:12:15 +0000

    removed unused imports

--- src/components/userAccount/Create.jsx
+++ src/components/userAccount/Create.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Service from "./Service";
 import Util from "../../util";
-import { response } from "express";
 
 export default function UserAccountCreate(props) {
   const [userAccount, setUserAccount] = useState({});
